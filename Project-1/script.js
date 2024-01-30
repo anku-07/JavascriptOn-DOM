193ec85627138021a01c58e7ee48cdb165c3001e
anku-07 <adityamondal48765@gmail.com>
2024-01-30 23:12:39 +0530

    Add new features

--- Project-1/script.js
+++ Project-1/script.js
@@ -3,13 +3,19 @@ let istatus = document.querySelector("h5");
 let addFriend = document.querySelector("#add");
 
 let removeFriend = document.querySelector("#remove");
+let count = 0;
+
 
 addFriend.addEventListener("click",function(){
-    istatus.innerHTML = "Friends";
-    istatus.style.color = "green"
+    if(count == 0){
+        istatus.style.color = "green"
+        istatus.innerHTML = "Friends🥳";
+        addFriend.innerHTML = "Remove Friend"
+        count = 1;
+    }else{
+        istatus.style.color = "red"
+        istatus.innerHTML = "Stranger😏";
+        addFriend.innerHTML = "Add Friend"
+        count = 0;
+    }
 });
-
-removeFriend.addEventListener("click",function(){
-    istatus.innerHTML = "Stranger";
-    istatus.style.color = "red"
-})
