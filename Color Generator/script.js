5210cefb8fe2c5e3dd0fc7983ac22e90a6f5cedd
anku-07 <adityamondal48765@gmail.com>
2024-02-01 22:26:18 +0530

    Add new feature

--- Color Generator/script.js
+++ Color Generator/script.js
@@ -4,3 +4,11 @@ let h3 = document.querySelector("h3")
 btn.addEventListener("click",function(){
     h3.style.color = "red";
 })
+
+function getColor(){
+    let red = math.floor(math.random() * 255);
+    let green = math.floor(math.random() * 255);
+    let blue = math.floor(math.random() * 255);
+    let color = `Color is ${red} , ${blue} , ${green}`
+    console.log(color);
+}
